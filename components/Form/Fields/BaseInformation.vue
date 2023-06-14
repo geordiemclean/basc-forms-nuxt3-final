21bf3a2bd64f1c40d8d72490bb0d2f557fe1b185
geordiemclean <68690354+geordiemclean@users.noreply.github.com>
2023-06-14 13:23:35 +1000

    added new base information

--- components/Form/Fields/BaseInformation.vue
+++ components/Form/Fields/BaseInformation.vue
@@ -1,13 +1,7 @@
  <template>
-    <div>
-    <v-row  v-for="(row, i) in childItem.tableRows" :key="i+45566">
-        <v-col cols="12">
-          <h1 class="font-weight-bold blue--text" v-if="row.type === 'Heading'">{{row.info}}</h1>
-          <p class="font-weight-bold black--text text-h6" v-if="row.type === 'Paragraph'">{{row.info}}</p>
-          <a class="font-weight-bold text-h6 blue--text"  target="_blank" v-if="row.type === 'Link'" :href="row.info">{{row.info}}</a>
-        </v-col>
-     </v-row>
-    </div>
+   <div>
+      <div class="mt-10" v-html="childItem.description"></div>
+       </div>
  </template>
  <script>
 
